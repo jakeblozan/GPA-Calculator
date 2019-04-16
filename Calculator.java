d4e9e3f5ea38f8cefcbe50b4e0038627132e0b4e
Jake Blozan <jakeblozan@users.noreply.github.com>
2019-04-15 21:12:46 -0700

    college update

--- Calculator.java
+++ Calculator.java
@@ -20,32 +20,88 @@ public class Calculator
         System.out.println("Welcome to the GPA calculator!");
         System.out.println("Please make sure you've filled out the grade form.\n");
 
-        while (input != "weighted" || input != "unweighted") {
-            System.out.println("Would you like to calculate your weighted or unweighted GPA?");
-            input = scan.next();
-            if (input.equalsIgnoreCase("weighted") || input.equalsIgnoreCase("unweighted")) {
-                System.out.println("Nice. Fetching data...");
-                break;
-            }
-            else {
-                System.out.println("What the... please try again and use a valid input");
-            }
+        /*while (input != "weighted" || input != "unweighted") {
+        System.out.println("Would you like to calculate your weighted or unweighted GPA?");
+        input = scan.next();
+        if (input.equalsIgnoreCase("weighted") || input.equalsIgnoreCase("unweighted")) {
+        System.out.println("Nice. Fetching data...");
+        break;
         }
+        else {
+        System.out.println("What the... please try again and use a valid input");
+        }
+        }*/
 
-        Scanner dataScan = new Scanner(new File("grades.txt"));
+        Scanner dataScan = new Scanner(new File("CP-grades.txt"));
         String data = "";
+
+        System.out.println("Nice. Fetching data...");
+
         while (dataScan.hasNext()) {
             data += dataScan.nextLine();
         }
 
-        if (input.equalsIgnoreCase("weighted")) {
-            weighted(data);
+        unweighted(data);
+
+        /*if (input.equalsIgnoreCase("weighted")) {
+        weighted(data);
         }
 
         if (input.equalsIgnoreCase("unweighted")) {
-            unweighted(data);
-        }
+        unweighted(data);
+        }*/
         //System.out.println(data);
+    }
+
+    public static void unweighted(String data) {
+        ArrayList<String> classes = new ArrayList<String>();
+        char[] grades = {'F', 'D', 'C', 'B', 'A'};
+        String line = "";
+        for (int i = 0; i < data.length(); i++) {
+            //System.out.print(data.charAt(i));
+            if (data.charAt(i) != ';') {
+                line += data.charAt(i);
+            }
+            else {
+                classes.add(line);
+                //System.out.println(line);
+                line = "";
+            }
+        }
+        classes.remove(0);
+        //System.out.println("\n\n" + Arrays.toString(classes.toArray()));
+        double pointsEarned = 0;
+        int units = 0;
+        for (int i = 0; i < classes.size(); i++) {
+            int spot = classes.get(i).indexOf(":");
+            int unit = Integer.parseInt(classes.get(i).substring(spot + 1, spot + 2));
+            String grade = classes.get(i).substring(spot + 3, spot + 5);
+            //System.out.println(unit + " " + grade);
+            units += unit;
+            for (int q = 0; q < 5; q++) {
+                if (grade.charAt(0) == grades[q]) {
+                    double pointsGrade = q * unit;
+                    if (grade.charAt(1) == '+') {
+                        pointsGrade += 1.2;
+                    }
+                    if (grade.charAt(1) == '-') {
+                        pointsGrade -= 1.2;
+                    }
+                    //System.out.println(pointsGrade);
+                    pointsEarned += pointsGrade;
+                }
+            }
+        }
+        double gpa = pointsEarned / units;
+        DecimalFormat numberFormat = new DecimalFormat("#.00");
+        System.out.println("Your unweighted GPA is: " + numberFormat.format(gpa));
+
+        //DEBUG
+        // System.out.println(pointsEarned);
+        // System.out.println(units);
+
+        // System.out.println(Arrays.toString(classes.toArray()));
+        // System.out.println(classes.get(0));
     }
 
     public static void weighted(String data) {
@@ -63,63 +119,26 @@ public class Calculator
             }
         }
         classes.remove(0);
-        double sum = 0;
+        double points = 0;
         for (int i = 0; i < classes.size(); i++) {
             int spot = classes.get(i).indexOf(":");
             char grade = classes.get(i).charAt(spot + 1);
             for (int q = 0; q < 5; q++) {
                 if (grade == grades[q]) {
-                    sum += q;
+                    points += q;
                 }
             }
             if (classes.get(i).charAt(spot + 2) == '.') {
-                sum++;
+                points++;
             }
         }
-        double gpa = sum / classes.size();
+        double gpa = points / classes.size();
         DecimalFormat numberFormat = new DecimalFormat("#.00");
         System.out.println("Your weighted GPA is: " + numberFormat.format(gpa));
-        
-        //System.out.println(sum);
-        //System.out.println(classes.size());
-        
-        //System.out.println(Arrays.toString(classes.toArray()));
-        //System.out.println(classes.get(0));
-    }
 
-    public static void unweighted(String data) {
-        ArrayList<String> classes = new ArrayList<String>();
-        char[] grades = {'F', 'D', 'C', 'B', 'A'};
-        String line = "";
-        for (int i = 0; i < data.length(); i++) {
-            if (data.charAt(i) != ';') {
-                line += data.charAt(i);
-            }
-            else {
-                //System.out.println(line);
-                classes.add(line);
-                line = "";
-            }
-        }
-        classes.remove(0);
-        double sum = 0;
-        for (int i = 0; i < classes.size(); i++) {
-            int spot = classes.get(i).indexOf(":");
-            char grade = classes.get(i).charAt(spot + 1);
-            for (int q = 0; q < 5; q++) {
-                if (grade == grades[q]) {
-                    sum += q;
-                }
-            }
-        }
-        double gpa = sum / classes.size();
-        DecimalFormat numberFormat = new DecimalFormat("#.00");
-        System.out.println("Your unweighted GPA is: " + numberFormat.format(gpa));
-        
-        //DEBUG
-        //System.out.println(sum);
+        //System.out.println(points);
         //System.out.println(classes.size());
-        
+
         //System.out.println(Arrays.toString(classes.toArray()));
         //System.out.println(classes.get(0));
     }
